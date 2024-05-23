b8357104391b91e2c70ce0b341460a7819898183
Phillip Sussman <phillipsussman@gmail.com>
2024-05-23 13:46:56 -0700

    Added GET request into the Stock controller.

--- FinShark/Controllers/StockController.cs
+++ FinShark/Controllers/StockController.cs
@@ -1,5 +1,25 @@
-﻿namespace FinShark.Controllers;
+﻿using FinShark.Data;
+using Microsoft.AspNetCore.Mvc;
+// ReSharper disable ConvertToPrimaryConstructor
 
-public class StockController
+namespace FinShark.Controllers;
+
+[Route("api/stock")]
+[ApiController]
+public class StockController : ControllerBase
 {
+    private readonly ApplicationDbContext _context;
+
+    public StockController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var stocks = _context.Stocks.ToList();
+
+        return Ok(stocks);
+    }
 }
