9fe83accce220015f1aadc2891405d3b0bd990e3
Phillip Sussman <phillipsussman@gmail.com>
2024-05-23 13:21:42 -0700

    Created new properties for the 2 models.

--- FinShark/Models/Comment.cs
+++ FinShark/Models/Comment.cs
@@ -2,5 +2,14 @@
 
 public class Comment
 {
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public DateTime CreatedOn { get; set; } = DateTime.Now;
 
+    // Foreign Key Setup
+    public int? StockId { get; set; }
+
+    // Navigation Property
+    public Stock? Stock { get; set; }
 }
