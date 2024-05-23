74456b98dd0dd8fe69e579e2c0d1ab8f76b6d9a9
Phillip Sussman <phillipsussman@gmail.com>
2024-05-23 13:11:57 -0700

    Add project files.

---
+++ FinShark/Models/Stock.cs
@@ -0,0 +1,9 @@
+﻿namespace FinShark.Models;
+
+public class Stock
+{
+    public int Id { get; set; }
+    public string Symbol { get; set; } = String.Empty;
+    public string CompanyName { get; set; } = string.Empty;
+
+}
